7694813de6471238a08db0b996bcdb0710054f44
Alexandra Schetelich <a.schetelich@gmx.net>
2026-09-14 09:06:25 +0200

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -50,13 +50,16 @@ export function Home() {
             Webentwicklung mit Blick für Struktur, Gestaltung und Nutzer.
           </h2> */}
 
-          <p className="mt-6 max-w-4xl text-text-muted">
-            Digitale Produkte überzeugen mich dann, wenn sie komplexe Aufgaben einfach machen.
-
-            Ich bin Alexandra Schetelich, Medieninformatikerin (M.Sc.) und Entwicklerin moderner Anwendungen.
-            Mein Schwerpunkt liegt auf der Konzeption und Entwicklung von Mobile Apps sowie in der KI-gestützten Entwicklung digitaler Lösungen.
-            Dabei verbinde ich technisches Verständnis mit einem Blick für klare und intuitive Anwendungen.
-            Mein Anspruch ist es, durchdachte und benutzerfreundliche Lösungen zu schaffen, die nicht nur funktionieren, sondern Menschen im Alltag wirklich unterstützen.</p>
+          <div className="mt-6 max-w-4xl text-text-muted">
+            <p> Digitale Produkte überzeugen mich dann, wenn sie komplexe Aufgaben einfach machen.
+            </p>
+            <p>
+              Ich bin Alexandra Schetelich, Medieninformatikerin (M.Sc.) und Entwicklerin moderner Anwendungen.
+              Mein Schwerpunkt liegt auf der Konzeption und Entwicklung von Mobile Apps sowie in der KI-gestützten Entwicklung digitaler Lösungen.
+              Dabei verbinde ich technisches Verständnis mit einem Blick für klare und intuitive Anwendungen.
+              Mein Anspruch ist es, durchdachte und benutzerfreundliche Lösungen zu schaffen, die nicht nur funktionieren, sondern Menschen im Alltag wirklich unterstützen.
+            </p>
+          </div>
 
           <div className="mt-8 flex flex-wrap gap-4">
             <a
